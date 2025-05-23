03c904f9d180002a8a3b0e440c14ddd0723b6855
Alex <alexcioba04@gmail.com>
2025-05-23 09:27:17 +0000

    phase 2 final

--- treasure_hub.c
+++ treasure_hub.c
@@ -12,49 +12,76 @@ pid_t pid = -1;
 void handler(int sig)
 {
     if (sig == SIGUSR1)
-    {
+      {
         int f = open("command.txt", O_RDONLY);
-
+	if(f < 0)
+	  {
+	    perror("Eroare deschidere command.txt");
+	    return;
+	  }
+	
         char buffer[100];
         int len = read(f, buffer, sizeof(buffer) - 1);
+	if(len < 0)
+	  {
+	    perror("Eroare citire command.txt");
+	    close(f);
+	    return;
+	  }
         close(f);
 
         buffer[len] = '\0';
 
-        char cmd[30], param1[30], param2[30];
+        char cmd[35], param1[35], param2[35];
         int args = sscanf(buffer, "%s %s %s", cmd, param1, param2);
+	
 
-        if (strcmp(cmd, "list_treasures") == 0 && args == 2)
+	pid_t nepotul = fork();
+	if(nepotul == 0)
 	  {
-            char path[100];
-            snprintf(path, sizeof(path), "./treasure_manager --list %s", param1);
-            system(path);
-            unlink("command.txt");
-            return;
-        }
+	    if (strcmp(cmd, "list_treasures") == 0 && args == 2)
+	      {
+		execl("./treasure_manager", "treasure_manager", "--list", param1, (char *)NULL);
+		perror("Eroare list_treasures");
+		_exit(1);
+	      }
+	    
+	    if (strcmp(cmd, "view_treasure") == 0 && args == 3)
+	      {
+		execl("./treasure_manager", "treasure_manager", "--view", param1, param2, (char *)NULL);
+		perror("Eroare view_treasure");
+		_exit(1);
+	      }
+	    
+	    if (strcmp(cmd, "list_hunts") == 0 && args == 1)
+	      {
+		execl("./treasure_manager", "treasure_manager", "--list_hunts", (char *)NULL);
+		perror("Eroare list_hunts");
+		_exit(1);
+	      }
 
-        if (strcmp(cmd, "view_treasure") == 0 && args == 3) {
-            char path[100];
-            snprintf(path, sizeof(path), "./treasure_manager --view %s %s", param1, param2);
-            system(path);
-            unlink("command.txt");
-            return;
-        }
-
-        if (strcmp(cmd, "list_hunts") == 0 && args == 1) {
-            system("./treasure_manager --list_hunts");
-            unlink("command.txt");
-            return;
-        }
-
-        printf("Comanda necunoscuta sau parametri lipsa: %s\n", buffer);
+	    printf("Comanda necunoscuta sau parametri lipsa: %s\n", buffer);
+	    _exit(1);
+	  }
+	else
+	  {
+	    if(nepotul > 0)
+	      {
+		waitpid(nepotul, NULL, 0);
+		unlink("command.txt");
+	      }
+	    else
+	      {
+		perror("Eroare la fork()");
+	      }
+	  }
     }
-
+    
     if (sig == SIGTERM)
-    {
-        printf("Monitorul se opreste, PID = %d\n", getpid());
-        exit(0);
-    }
+      {
+	write(STDOUT_FILENO, "Monitorul se opreste\n", 22);
+	exit(0);
+      }
 }
 
 
@@ -89,20 +116,24 @@ void process_command(const char* command_line)
     exit(0);
     }
   
-  if (pid > 0) {
-    int f = open("command.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (f < 0) {
-      perror("Eroare la open()");
-      return;
-    }
-        write(f, command_line, strlen(command_line) + 1);
-        close(f);
-        kill(pid, SIGUSR1);
-  } else {
-    printf("Monitorul nu este pornit\n");
+  if (pid > 0)
+    {
+      int f = open("command.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+      if (f < 0)
+	{
+	  perror("Eroare la open()");
+	  return;
+	}
+      write(f, command_line, strlen(command_line)+1);
+      fsync(f);
+      close(f);
+      kill(pid, SIGUSR1);
   }
+  else
+    {
+      printf("Monitorul nu este pornit\n");
+    }
 }
-
 
 
 
@@ -124,24 +155,23 @@ void start_monitor() {
       {
 	if(pid == 0) //copil
 	  {
-	    printf("Monitor pornit cu PID = %d\n", getpid());
-
-	    //definire semnale
 	    struct sigaction sa;
 	    sa.sa_handler = handler;
 	    sigemptyset(&sa.sa_mask);
-	    sa.sa_flags = 0;
+	    sa.sa_flags = SA_RESTART;
 
 	    sigaction(SIGUSR1, &sa, NULL);
 	    sigaction(SIGTERM, &sa, NULL);
 	    
 	    while(1)
 	      {
-		pause(); //apeleaza automat handle_signal() cand primeste semnal
+		pause();
 	      }
 	  }
 	else //parinte
 	  {
+	    printf("Monitor pornit cu PID = %d\n", pid);
+	    
 	    char command[100];
 	    while(1)
 	      {
